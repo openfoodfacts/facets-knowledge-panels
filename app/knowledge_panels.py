2e66e007bb9d8dd7e265ec4a62476ed4124ded80
Sumit Kashyap <mr.sumitkrr@gmail.com>
2022-07-15 15:20:40 +0530

    Update app/knowledge_panels.py
    Co-authored-by: Alex Garel <alex@garel.org>

--- app/knowledge_panels.py
+++ app/knowledge_panels.py
@@ -10,17 +10,27 @@ def hunger_game_kp(
     value: Union[str, None] = None,
     country: Union[str, None] = None,
 ):
-    baseUrl = "https://hunger.openfoodfacts.org/?type="
-    valueQuery = urlencode({"value_tag": value})
-    countryQuery = urlencode({"country": country})
-    if country == None and value != None:
-        html = f"<p><a href='{baseUrl}{hunger_game_filter}&{valueQuery}'></a></p>\n"
-    elif value == None and country != None:
-        html = f"<p><a href='{baseUrl}{hunger_game_filter}&{countryQuery}'></a></p>\n"
-    elif value == None and country == None:
-        html = f"<p><a href='{baseUrl}{hunger_game_filter}'></a></p>\n"
-    else:
-        html = f"<p><a href='{baseUrl}{hunger_game_filter}&{valueQuery}&{countryQuery}'></a></p>\n"
+    query = {}
+    description = ""
+    if hunger_game_filter == "country":
+        # superseed country parameter
+        country = value
+        # and do not use facet
+        hunger_game_filter = value = None
+    if country is not None:
+        query["country"] = country
+        description = country
+    if hunger_game_filter is not None:
+        query["type"] = hunger_game_filter
+        description = f"{hunger_game_filter}"
+    if value_tag is not None:
+        query["value_tag"] = value
+        description = f"{value} {hunger_game_filter}"
+    questions_url = "https://hunger.openfoodfacts.org/"
+    if query:
+        questions_url += f"?{urlencode(query)}"
+    description = f"Answer robotoff questions about {description}"
+    html = f"<p><a href='{questions_url}'>{description}</a></p>\n"
     return {
         "hunger-game": {
             "elements": [
