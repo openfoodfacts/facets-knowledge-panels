ebfc2e16752d11116b9aed2039a2c910443d5203
Sumit Kashyap <mr.sumitkrr@gmail.com>
2022-09-02 18:05:21 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -27,23 +27,34 @@ async def knowledge_panel(
     FacetName is the model that have list of values
     facet_value are the list of values connecting to FacetName eg:- category/beer, here beer is the value
     """
-    async with asyncer.create_task_group() as task_group:
-        with active_translation(lang_code):
+    with active_translation(lang_code):
+        async with asyncer.create_task_group() as task_group:
+
+            soon_value1 = task_group.soonify(hunger_game_kp)(
+                hunger_game_filter=facet_tag, value=value_tag, country=country
+            )
+
+            soon_value2 = task_group.soonify(data_quality_kp)(
+                facet=facet_tag, value=value_tag, country=country
+            )
+
+            soon_value3 = task_group.soonify(last_edits_kp)(
+                facet=facet_tag, value=value_tag, country=country
+            )
+        panels = []
+
+        try:
             if facet_tag in HungerGameFilter.list():
-                soon_value1 = task_group.soonify(hunger_game_kp)(
-                    hunger_game_filter=facet_tag, value=value_tag, country=country
-                )
-            try:
-                soon_value2 = task_group.soonify(data_quality_kp)(
-                    facet=facet_tag, value=value_tag, country=country
-                )
-            except Exception:
-                logging.exception("error occued while appending data-quality-kp")
-            try:
-                soon_value3 = task_group.soonify(last_edits_kp)(
-                    facet=facet_tag, value=value_tag, country=country
-                )
-            except Exception:
-                logging.exception("error occued while appending last-edits-kp")
-    panels = [soon_value1.value, soon_value2.value, soon_value3.value]
-    return {"knowledge_panels": panels}
+                panels.append(soon_value1.value)
+        except Exception:
+            logging.exception("Hungergame filter doesn't exists")
+        try:
+            panels.append(soon_value2.value)
+        except Exception:
+            logging.exception("error occued while appending data_quality_kp")
+        try:
+            panels.append(soon_value3.value)
+        except Exception:
+            logging.exception("error occued while appending last_edits_kp")
+
+        return {"knowledge_panels": panels}
