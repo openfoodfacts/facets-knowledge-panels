ca0afa140a11455794f525fd59e213ea85495f46
Alex Garel <alex@garel.org>
2022-09-02 17:06:26 +0200

    refactor: conform to quality checks

--- tests/test_knowledge_panels.py
+++ tests/test_knowledge_panels.py
@@ -18,15 +18,17 @@ def auto_activate_lang():
 
 
 def test_hunger_game_kp_with_filter_value_and_country():
-    assert hunger_game_kp(
-        hunger_game_filter="country", value="germany", country="france"
-    ) == {
+    html = (
+        "<p><a href='https://hunger.openfoodfacts.org/questions?country=germany'>"
+        "Answer robotoff questions about germany</a></p>\n"
+    )
+    assert hunger_game_kp(hunger_game_filter="country", value="germany", country="france") == {
         "hunger-game": {
             "elements": [
                 {
                     "element_type": "text",
                     "text_element": {
-                        "html": "<p><a href='https://hunger.openfoodfacts.org/questions?country=germany'>Answer robotoff questions about germany</a></p>\n"
+                        "html": html,
                     },
                 }
             ]
@@ -35,13 +37,17 @@ def test_hunger_game_kp_with_filter_value_and_country():
 
 
 def test_hunger_game_kp_with_category():
+    html = (
+        "<p><a href='https://hunger.openfoodfacts.org/questions?type=category'>"
+        "Answer robotoff questions about category</a></p>\n"
+    )
     assert hunger_game_kp(hunger_game_filter="category") == {
         "hunger-game": {
             "elements": [
                 {
                     "element_type": "text",
                     "text_element": {
-                        "html": "<p><a href='https://hunger.openfoodfacts.org/questions?type=category'>Answer robotoff questions about category</a></p>\n"
+                        "html": html,
                     },
                 }
             ]
@@ -50,13 +56,17 @@ def test_hunger_game_kp_with_category():
 
 
 def test_hunger_game_kp_category_with_country():
+    html = (
+        "<p><a href='https://hunger.openfoodfacts.org/questions?country=france&type=category'>"
+        "Answer robotoff questions about category</a></p>\n"
+    )
     assert hunger_game_kp(hunger_game_filter="category", country="france") == {
         "hunger-game": {
             "elements": [
                 {
                     "element_type": "text",
                     "text_element": {
-                        "html": "<p><a href='https://hunger.openfoodfacts.org/questions?country=france&type=category'>Answer robotoff questions about category</a></p>\n"
+                        "html": html,
                     },
                 }
             ]
@@ -65,13 +75,17 @@ def test_hunger_game_kp_category_with_country():
 
 
 def test_hunger_game_kp_category_with_value():
+    html = (
+        "<p><a href='https://hunger.openfoodfacts.org/questions?type=category&value_tag=beer'>"
+        "Answer robotoff questions about beer category</a></p>\n"
+    )
     assert hunger_game_kp(hunger_game_filter="category", value="beer") == {
         "hunger-game": {
             "elements": [
                 {
                     "element_type": "text",
                     "text_element": {
-                        "html": "<p><a href='https://hunger.openfoodfacts.org/questions?type=category&value_tag=beer'>Answer robotoff questions about beer category</a></p>\n"
+                        "html": html,
                     },
                 }
             ]
@@ -80,13 +94,17 @@ def test_hunger_game_kp_category_with_value():
 
 
 def test_hunger_game_kp_brand_with_value():
+    html = (
+        "<p><a href='https://hunger.openfoodfacts.org/questions?type=brand&value_tag=nestle'>"
+        "Answer robotoff questions about nestle brand</a></p>\n"
+    )
     assert hunger_game_kp(hunger_game_filter="brand", value="nestle") == {
         "hunger-game": {
             "elements": [
                 {
                     "element_type": "text",
                     "text_element": {
-                        "html": "<p><a href='https://hunger.openfoodfacts.org/questions?type=brand&value_tag=nestle'>Answer robotoff questions about nestle brand</a></p>\n"
+                        "html": html,
                     },
                 }
             ]
@@ -95,13 +113,17 @@ def test_hunger_game_kp_brand_with_value():
 
 
 def test_hunger_game_kp_label_with_value():
+    html = (
+        "<p><a href='https://hunger.openfoodfacts.org/questions?type=label&value_tag=organic'>"
+        "Answer robotoff questions about organic label</a></p>\n"
+    )
     assert hunger_game_kp(hunger_game_filter="label", value="organic") == {
         "hunger-game": {
             "elements": [
                 {
                     "element_type": "text",
                     "text_element": {
-                        "html": "<p><a href='https://hunger.openfoodfacts.org/questions?type=label&value_tag=organic'>Answer robotoff questions about organic label</a></p>\n"
+                        "html": html,
                     },
                 }
             ]
@@ -111,6 +133,7 @@ def test_hunger_game_kp_label_with_value():
 
 def test_data_quality_kp_with_country(monkeypatch):
     expected_url = "https://tr-en.openfoodfacts.org/data-quality.json"
+    base_url = "https://tr-en.openfoodfacts.org/data-quality"
     json_content = {
         "count": 125,
         "tags": [
@@ -119,47 +142,45 @@ def test_data_quality_kp_with_country(monkeypatch):
                 "known": 0,
                 "name": "ecoscore-production-system-no-label",
                 "products": 1583,
-                "url": "https://tr-en.openfoodfacts.org/data-quality/ecoscore-production-system-no-label",
+                "url": f"{base_url}/ecoscore-production-system-no-label",
             },
             {
                 "id": "en:no-packaging-data",
                 "known": 0,
                 "name": "no-packaging-data",
                 "products": 1531,
-                "url": "https://tr-en.openfoodfacts.org/data-quality/no-packaging-data",
+                "url": f"{base_url}/no-packaging-data",
             },
             {
                 "id": "en:ecoscore-origins-of-ingredients-origins-are-100-percent-unknown",
                 "known": 0,
                 "name": "ecoscore-origins-of-ingredients-origins-are-100-percent-unknown",
                 "products": 1515,
-                "url": "https://tr-en.openfoodfacts.org/data-quality/ecoscore-origins-of-ingredients-origins-are-100-percent-unknown",
+                "url": (
+                    f"{base_url}/" "ecoscore-origins-of-ingredients-origins-are-100-percent-unknown"
+                ),
             },
         ],
     }
 
-    monkeypatch.setattr(
-        requests, "get", mock_get_factory(expected_url, json_content=json_content)
-    )
-    result = app.main.data_quality_kp(
-        facet="country", value="Turkey", country="Hungary"
-    )
+    monkeypatch.setattr(requests, "get", mock_get_factory(expected_url, json_content=json_content))
+    result = app.main.data_quality_kp(facet="country", value="Turkey", country="Hungary")
     first_element = result["Quality"]["elements"][0]
     first_element["text_element"] = tidy_html(first_element["text_element"])
     expected_text = """
     <ul>
         <p>The total number of issues are 125</p>
         <li>
-            <a herf=https://tr-en.openfoodfacts.org/data-quality/ecoscore-production-system-no-label>1583 products with ecoscore-production-system-no-label</a>
+            <a href=https://tr-en.openfoodfacts.org/data-quality/ecoscore-production-system-no-label>1583 products with ecoscore-production-system-no-label</a>
         </li>
         <li>
-            <a herf=https://tr-en.openfoodfacts.org/data-quality/no-packaging-data>1531 products with no-packaging-data</a>
+            <a href=https://tr-en.openfoodfacts.org/data-quality/no-packaging-data>1531 products with no-packaging-data</a>
         </li>
         <li>
-            <a herf=https://tr-en.openfoodfacts.org/data-quality/ecoscore-origins-of-ingredients-origins-are-100-percent-unknown>1515 products with ecoscore-origins-of-ingredients-origins-are-100-percent-unknown</a>
+            <a href=https://tr-en.openfoodfacts.org/data-quality/ecoscore-origins-of-ingredients-origins-are-100-percent-unknown>1515 products with ecoscore-origins-of-ingredients-origins-are-100-percent-unknown</a>
         </li>
     </ul>
-    """
+    """  # noqa: E501  # allow long lines
     # assert html separately to have better output in case of error
     assert first_element["text_element"] == tidy_html(expected_text)
     # now replace it for concision of output
@@ -181,6 +202,7 @@ def test_data_quality_kp_with_country(monkeypatch):
 
 def test_data_quality_kp_with_all_three_values(monkeypatch):
     expected_url = "https://world.openfoodfacts.org/brand/lidl/data-quality.json"
+    base_url = "https://world.openfoodfacts.org/brand/lidl/data-quality"
     json_content = {
         "count": 182,
         "tags": [
@@ -189,28 +211,28 @@ def test_data_quality_kp_with_all_three_values(monkeypatch):
                 "known": 0,
                 "name": "ecoscore-origins-of-ingredients-origins-are-100-percent-unknown",
                 "products": 7688,
-                "url": "https://world.openfoodfacts.org/brand/lidl/data-quality/ecoscore-origins-of-ingredients-origins-are-100-percent-unknown",
+                "url": (
+                    f"{base_url}/" "ecoscore-origins-of-ingredients-origins-are-100-percent-unknown"
+                ),
             },
             {
                 "id": "en:ecoscore-production-system-no-label",
                 "known": 0,
                 "name": "ecoscore-production-system-no-label",
                 "products": 7661,
-                "url": "https://world.openfoodfacts.org/brand/lidl/data-quality/ecoscore-production-system-no-label",
+                "url": f"{base_url}/ecoscore-production-system-no-label",
             },
             {
                 "id": "en:no-packaging-data",
                 "known": 0,
                 "name": "no-packaging-data",
                 "products": 6209,
-                "url": "https://world.openfoodfacts.org/brand/lidl/data-quality/no-packaging-data",
+                "url": f"{base_url}/no-packaging-data",
             },
         ],
     }
 
-    monkeypatch.setattr(
-        requests, "get", mock_get_factory(expected_url, json_content=json_content)
-    )
+    monkeypatch.setattr(requests, "get", mock_get_factory(expected_url, json_content=json_content))
     result = app.main.data_quality_kp(facet="brand", value="lidl")
     first_element = result["Quality"]["elements"][0]
     first_element["text_element"] = tidy_html(first_element["text_element"])
@@ -218,16 +240,16 @@ def test_data_quality_kp_with_all_three_values(monkeypatch):
     <ul>
         <p>The total number of issues are 182</p>
         <li>
-            <a herf=https://world.openfoodfacts.org/brand/lidl/data-quality/ecoscore-origins-of-ingredients-origins-are-100-percent-unknown>7688 products with ecoscore-origins-of-ingredients-origins-are-100-percent-unknown</a>
+            <a href=https://world.openfoodfacts.org/brand/lidl/data-quality/ecoscore-origins-of-ingredients-origins-are-100-percent-unknown>7688 products with ecoscore-origins-of-ingredients-origins-are-100-percent-unknown</a>
         </li>
         <li>
-            <a herf=https://world.openfoodfacts.org/brand/lidl/data-quality/ecoscore-production-system-no-label>7661 products with ecoscore-production-system-no-label</a>
+            <a href=https://world.openfoodfacts.org/brand/lidl/data-quality/ecoscore-production-system-no-label>7661 products with ecoscore-production-system-no-label</a>
         </li>
         <li>
-            <a herf=https://world.openfoodfacts.org/brand/lidl/data-quality/no-packaging-data>6209 products with no-packaging-data</a>
+            <a href=https://world.openfoodfacts.org/brand/lidl/data-quality/no-packaging-data>6209 products with no-packaging-data</a>
         </li>
     </ul>
-    """
+    """  # noqa: E501  # allow long lines
     # assert html separately to have better output in case of error
     assert first_element["text_element"] == tidy_html(expected_text)
     # now replace it for concision of output
@@ -279,9 +301,7 @@ def test_last_edits_kp_with_all_three_values(monkeypatch):
             json_content,
         ),
     )
-    result = app.main.last_edits_kp(
-        facet="vitamin", value="vitamin-k", country="hungary"
-    )
+    result = app.main.last_edits_kp(facet="vitamin", value="vitamin-k", country="hungary")
     first_element = result["LastEdits"]["elements"][0]
     first_element["text_element"] = tidy_html(first_element["text_element"])
     last_edits_text = """
@@ -300,7 +320,9 @@ def test_last_edits_kp_with_all_three_values(monkeypatch):
         "LastEdits": {
             "title": "Last-edits",
             "subtitle": "last-edits issues related to hungary vitamin vitamin-k",
-            "source_url": "https://hu-en.openfoodfacts.org/vitamin/vitamin-k?sort_by=last_modified_t",
+            "source_url": (
+                "https://hu-en.openfoodfacts.org/vitamin/vitamin-k?sort_by=last_modified_t"
+            ),
             "elements": [
                 {
                     "element_type": "text",
@@ -333,12 +355,14 @@ def test_wikidata_kp(monkeypatch):
     )
     # then mock the call to wikidata
     # fake entity mimicks the Entity object from wikidata library
+    image_url = (
+        "https://upload.wikimedia.org/wikipedia/commons/d/d6/"
+        "Paziols_%28France%29_Vue_du_village.jpg"
+    )
     fake_entity = {
         "description": {"en": "French wine appellation", "fr": "région viticole"},
         "label": {"en": "Fitou AOC", "fr": "Fitou"},
-        wikidata_props.image_prop: DictAttr(
-            image_url="https://upload.wikimedia.org/wikipedia/commons/d/d6/Paziols_%28France%29_Vue_du_village.jpg"
-        ),
+        wikidata_props.image_prop: DictAttr(image_url=image_url),
         wikidata_props.OSM_prop: "2727716",
         wikidata_props.INAO_prop: "6159",
         "attributes": {
@@ -364,7 +388,7 @@ def test_wikidata_kp(monkeypatch):
                 {
                     "element_type": "text",
                     "text_element": "Fitou AOC",
-                    "image_url": "https://upload.wikimedia.org/wikipedia/commons/d/d6/Paziols_%28France%29_Vue_du_village.jpg",
+                    "image_url": image_url,
                 },
                 {
                     "element_type": "links",
@@ -391,7 +415,7 @@ def test_wikidata_kp(monkeypatch):
                     {
                         "element_type": "text",
                         "text_element": "Fitou",
-                        "image_url": "https://upload.wikimedia.org/wikipedia/commons/d/d6/Paziols_%28France%29_Vue_du_village.jpg",
+                        "image_url": image_url,
                     },
                     {
                         "element_type": "links",
