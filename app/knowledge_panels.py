openfoodfacts/facets-knowledge-panels

commit 47d0c1b6e4805b21453c314e23b927af57867c8c

fix: return translated description in Hunger Games KP given valid lang_code (#175)
- This PR fixes #174 following the suggestion I mentioned in the issue.
- Also added wrap_text util function 
- Also added the required translatable strings in knowledge-panel.pot so
that all description (depending on what query parameters were provided)
are covered and translated properly (checked each combination).
- Modified tests.

app/knowledge_panels.py
@@ -14,6 +14,7 @@ from .models import (
     singularize,
 )
 from .off import data_quality, last_edit, wikidata_helper
+from .utils import wrap_text
 
 
 class KnowledgePanels:
@@ -39,23 +40,34 @@ class KnowledgePanels:
         facets = {k: v for k, v in facets.items() if k is not None and k in HungerGameFilter.list()}
         urls = set()
         descriptions = collections.OrderedDict()
+        description_values = dict()
         html = []
         # a country is specified
         if self.country is not None:
             query["country"] = f"en:{self.country}"
-            descriptions["country"] = f"for country {self.country}"
+            descriptions["country"] = "for the country - {country}"
+            description_values["country"] = self.country.capitalize()
+
         # one of the facet is a country, use it as a country parameter
         if facets.get("country"):
             country_value = facets.pop("country")
             # remove eventual prefix
             country_value = country_value.split(":", 1)[-1]
             query["country"] = f"en:{country_value}"
-            descriptions["country"] = f"for country {country_value}"
+            descriptions["country"] = "for the country - {country}"
+            description_values["country"] = country_value.capitalize()
+
         # brand can be used as a filter, if we have more than one facet
         if facets.get("brand") and len(facets) > 1:
             brand_value = facets.pop("brand")
             query["brand"] = brand_value
-            descriptions["brand"] = f"for brand {brand_value}"
+            descriptions["brand"] = "and the {brand_name} brand"
+            description_values["brand_name"] = wrap_text(brand_value, "single_quotes")
+
+        if descriptions.get("country"):
+            descriptions.move_to_end(
+                "country"
+            )  # so that 'for the country - <country name>' is always at the end.
         description = " ".join(descriptions.values())
         # generate an url for each remaining facets
         for k, v in facets.items():
@@ -65,26 +77,39 @@ class KnowledgePanels:
             facet_description = k
             if v is not None:
                 facet_query["value_tag"] = v
-                value_description += v
+                value_description += wrap_text(v, "single_quotes")
             if value_description:
                 value_description += " "
             urls.add(
                 (
                     f"{questions_url}?{urlencode(facet_query)}",
-                    f"about {facet_description} {value_description}{description}".strip(),
+                    f"about the {{facet_value}}{{facet_name}} {description}".strip(),
+                    (facet_description, value_description),
                 )
             )
         if query:
-            urls.add((f"{questions_url}?{urlencode(query)}", description))
+            if descriptions.get("brand"):
+                description = description.replace(
+                    "and", "about", 1
+                )  # So that its "Answer robotoff questions about the <brand_name> ..."
+                # and not "Answer robotoff questions and the <brand_name> ..."
+                # when two facets are given (including brand)
+            urls.add((f"{questions_url}?{urlencode(query)}", description, (None, None)))
 
-        t_description = _("Answer robotoff questions")
+        t_description = "Answer robotoff questions"
         for id, val in enumerate(sorted(urls)):
-            url, des = val
+            url, des, (facet_name, facet_value) = val
+            final_description = _(f"{t_description} {des}").format(
+                brand_name=description_values.get("brand_name"),
+                country=description_values.get("country"),
+                facet_name=facet_name,
+                facet_value=facet_value,
+            )
             html.append(
                 {
                     "element_type": "text",
                     "text_element": {
-                        "html": f"<ul><li><p><a href='{url}'><em>{t_description} {des}</em>"
+                        "html": f"<ul><li><p><a href='{url}'><em>{final_description}</em>"
                         + "</a></p></li></ul>"
                     },
                 },

i18n/knowledge-panel.pot
@@ -50,3 +50,36 @@ msgstr ""
 #: app/off.py:91
 msgid "Answer robotoff questions about"
 msgstr ""
+
+#: The next 7 robotoff strings are in app/knowledge_panels.py:106
+#, python-brace-format
+msgid "Answer robotoff questions for the country - {country}"
+msgstr ""
+
+#, python-brace-format
+msgid "Answer robotoff questions about the {brand_name} brand"
+msgstr ""
+
+#, python-brace-format
+msgid "Answer robotoff questions about the {brand_name} brand for the country - {country}"
+msgstr ""
+
+#: Eg: Answer robotoff questions about the 'beers' category (facet_value = beers, facet_name = category)
+#: Note: No need to add single quotes to facet_value, it's handled internally.
+#, python-brace-format
+msgid "Answer robotoff questions about the {facet_value}{facet_name}"
+msgstr ""
+
+#: Eg: Answer robotoff questions about the 'beers' category about the 'keystone' brand(facet_value = beers, facet_name = category, brand_name=keystone)
+#: Note: No need to add single quotes to facet_value, it's handled internally.
+#, python-brace-format
+msgid "Answer robotoff questions about the {facet_value}{facet_name} and the {brand_name} brand"
+msgstr ""
+
+#, python-brace-format
+msgid "Answer robotoff questions about the {facet_value}{facet_name} for the country - {country}"
+msgstr ""
+
+#, python-brace-format
+msgid "Answer robotoff questions about the {facet_value}{facet_name} and the {brand_name} brand for the country - {country}"
+msgstr ""

app/utils.py
@@ -1,0 +1,17 @@
+def wrap_text(text, wrapper):
+    """
+    Utility function to wrap the given text in the given wrapper.
+    E.g 'text' if wrapper = single_quotes
+    """
+    if wrapper == "single_quotes":
+        return f"'{text}'"
+    elif wrapper == "quotes":
+        return f'"{text}"'
+    elif wrapper == "bold":
+        return f"<b>{text}</b>"
+    elif wrapper == "italic":
+        return f"<i>{text}</i>"
+    elif wrapper == "underline":
+        return f"<u>{text}</u>"
+    else:
+        return text  # if given wrapper isn't valid then return the given text.

tests/test_hunger_game_kp.py
@@ -28,7 +28,7 @@ async def test_hunger_game_kp_with_filter_value_and_country():
     """test_hunger_game_kp_with_filter_value_and_country"""
     html = (
         "<ul><li><p><a href='https://hunger.openfoodfacts.org/questions?country=en%3Agermany'>"
-        "<em>Answer robotoff questions for country germany</em></a></p></li></ul>"
+        "<em>Answer robotoff questions for the country - Germany</em></a></p></li></ul>"
     )
     result = await KnowledgePanels(
         facet="country", value="germany", country="france"
@@ -45,7 +45,7 @@ async def test_hunger_game_kp_with_category():
     """test_hunger_game_kp_with_category"""
     html = (
         "<ul><li><p><a href='https://hunger.openfoodfacts.org/questions?type=category'>"
-        "<em>Answer robotoff questions about category</em></a></p></li></ul>"
+        "<em>Answer robotoff questions about the category</em></a></p></li></ul>"
     )
     result = await KnowledgePanels(facet="category").hunger_game_kp()
     assert result == {
@@ -60,12 +60,12 @@ async def test_hunger_game_kp_category_with_country():
     """test_hunger_game_kp_category_with_country"""
     html0 = (
         "<ul><li><p><a href='https://hunger.openfoodfacts.org/questions?country=en%3Afrance'>"
-        "<em>Answer robotoff questions for country france</em></a></p></li></ul>"
+        "<em>Answer robotoff questions for the country - France</em></a></p></li></ul>"
     )
     html1 = (
         "<ul><li><p>"
         "<a href='https://hunger.openfoodfacts.org/questions?country=en%3Afrance&type=category'>"
-        "<em>Answer robotoff questions about category for country france"
+        "<em>Answer robotoff questions about the category for the country - France"
         "</em></a></p></li></ul>"
     )
     result = await KnowledgePanels(facet="category", country="france").hunger_game_kp()
@@ -86,7 +86,7 @@ async def test_hunger_game_kp_category_with_value():
         "<ul><li><p><a href='https://hunger.openfoodfacts.org/"
         "questions?type=category&value_tag=en%3Abeers'>"
         # noqa: E501
-        "<em>Answer robotoff questions about category en:beers</em></a></p></li></ul>"
+        "<em>Answer robotoff questions about the 'en:beers' category</em></a></p></li></ul>"
     )
     result = await KnowledgePanels(facet="category", value="en:beers").hunger_game_kp()
     assert result == {
@@ -102,7 +102,7 @@ async def test_hunger_game_kp_brand_with_value():
     html = (
         "<ul><li><p><a href='https://hunger.openfoodfacts.org/"
         "questions?type=brand&value_tag=nestle'>"
-        "<em>Answer robotoff questions about brand nestle</em></a></p></li></ul>"
+        "<em>Answer robotoff questions about the 'nestle' brand</em></a></p></li></ul>"
     )
     result = await KnowledgePanels(facet="brand", value="nestle").hunger_game_kp()
     assert result == {
@@ -118,7 +118,7 @@ async def test_hunger_game_kp_label_with_value():
     html = (
         "<ul><li><p><a href='https://hunger.openfoodfacts.org/"
         "questions?type=label&value_tag=en%3Aorganic'>"
-        "<em>Answer robotoff questions about label en:organic</em></a></p></li></ul>"
+        "<em>Answer robotoff questions about the 'en:organic' label</em></a></p></li></ul>"
     )
     result = await KnowledgePanels(facet="label", value="en:organic").hunger_game_kp()
     assert result == {
@@ -134,7 +134,7 @@ async def test_hunger_game_kp_label_with_value_plural_facet():
     html = (
         "<ul><li><p><a href='https://hunger.openfoodfacts.org/"
         "questions?type=label&value_tag=en%3Aorganic'>"
-        "<em>Answer robotoff questions about label en:organic</em></a></p></li></ul>"
+        "<em>Answer robotoff questions about the 'en:organic' label</em></a></p></li></ul>"
     )
     result = await KnowledgePanels(facet="labels", value="en:organic").hunger_game_kp()
     assert result == {
@@ -150,13 +150,13 @@ async def test_HungerGame_double_country_and_value():
     # facet country have priority
     html1 = (
         "<ul><li><p><a href='https://hunger.openfoodfacts.org/questions?country=en%3Afrance'>"
-        "<em>Answer robotoff questions for country france</em></a></p></li></ul>"
+        "<em>Answer robotoff questions for the country - France</em></a></p></li></ul>"
     )
     html2 = (
         "<ul><li><p><a href='https://hunger.openfoodfacts.org/"
         "questions?country=en%3Afrance&type=category&value_tag=beers'>"
         # noqa:E501
-        "<em>Answer robotoff questions about category beers for country france"
+        "<em>Answer robotoff questions about the 'beers' category for the country - France"
         "</em></a></p></li></ul>"
     )
     kp = KnowledgePanels(
@@ -193,14 +193,14 @@ async def test_hunger_game_kp_with_all_tag_1():
     html0 = (
         "<ul><li><p><a href='https://hunger.openfoodfacts.org/"
         "questions?country=en%3Afrance&brand=lidl'>"
-        "<em>Answer robotoff questions for country france for brand lidl"
+        "<em>Answer robotoff questions about the 'lidl' brand for the country - France"
         "</em></a></p></li></ul>"
     )
     html1 = (
         "<ul><li><p><a href='https://hunger.openfoodfacts.org/"
         "questions?country=en%3Afrance&brand=lidl&type=category&value_tag=en%3Abeers'>"
-        "<em>Answer robotoff questions about category en:beers for country france for brand lidl"
-        "</em></a></p></li></ul>"  # noqa: E501
+        "<em>Answer robotoff questions about the 'en:beers' category "
+        "and the 'lidl' brand for the country - France</em></a></p></li></ul>"  # noqa: E501
     )
     assert await KnowledgePanels(
         facet="category",
@@ -223,13 +223,13 @@ async def test_hunger_game_kp_with_all_tag_2():
     """test_hunger_game_kp_with_all_tag_2"""
     html0 = (
         "<ul><li><p><a href='https://hunger.openfoodfacts.org/questions?type=brand'>"
-        "<em>Answer robotoff questions about brand</em></a></p></li></ul>"
+        "<em>Answer robotoff questions about the brand</em></a></p></li></ul>"
     )
     html1 = (
         "<ul><li><p><a href='https://hunger.openfoodfacts.org/"
         "questions?type=category&value_tag=en%3Acoffees'>"
         # noqa: E501
-        "<em>Answer robotoff questions about category en:coffees</em></a></p></li></ul>"
+        "<em>Answer robotoff questions about the 'en:coffees' category</em></a></p></li></ul>"
     )
     assert await KnowledgePanels(
         facet="brand",
@@ -250,20 +250,20 @@ async def test_hunger_game_kp_with_all_tag_3():
     """test_hunger_game_kp_with_all_tag_3"""
     html0 = (
         "<ul><li><p><a href='https://hunger.openfoodfacts.org/questions?country=en%3Aitaly'>"
-        "<em>Answer robotoff questions for country italy</em></a></p></li></ul>"
+        "<em>Answer robotoff questions for the country - Italy</em></a></p></li></ul>"
     )
     html1 = (
         "<ul><li><p><a href='https://hunger.openfoodfacts.org/"
         "questions?country=en%3Aitaly&type=category&value_tag=en%3Ameals'>"
         # noqa: E501
-        "<em>Answer robotoff questions about category en:meals for country italy</em></a>"
-        "</p></li></ul>"
+        "<em>Answer robotoff questions about the 'en:meals' category "
+        "for the country - Italy</em></a></p></li></ul>"
     )
     html2 = (
         "<ul><li><p><a href='https://hunger.openfoodfacts.org/"
         "questions?country=en%3Aitaly&type=label&value_tag=vegan'>"
         # noqa: E501
-        "<em>Answer robotoff questions about label vegan for country italy</em></a>"
+        "<em>Answer robotoff questions about the 'vegan' label for the country - Italy</em></a>"
         "</p></li></ul>"
     )
     assert await KnowledgePanels(

commit e1610dc76030ca3f01764a53bf4b90ef1fa4cde0

Resolve bug introduced in #175

app/knowledge_panels.py
@@ -81,13 +81,11 @@ class KnowledgePanels:
             facet_description = k
             if v is not None:
                 facet_query["value_tag"] = v
-                value_description += wrap_text(v, "single_quotes")
-            if value_description:
-                value_description += " "
+                value_description = v
             urls.add(
                 (
                     f"{questions_url}?{urlencode(facet_query)}",
-                    f"about the {{facet_value}}{{facet_name}} {description}".strip(),
+                    f"about the {{facet_value}} {{facet_name}} {description}".strip(),
                     (facet_description, value_description),
                 )
             )
@@ -103,11 +101,14 @@ class KnowledgePanels:
         t_description = "Answer questions from Robotoff"
         for id, val in enumerate(sorted(urls)):
             url, des, (facet_name, facet_value) = val
-            final_description = _(f"{t_description} {des}").format(
-                brand_name=description_values.get("brand_name"),
-                country=description_values.get("country"),
-                facet_name=facet_name,
-                facet_value=facet_value,
+            final_description = format_translation(
+                _(f"{t_description} {des}"),
+                values={
+                    "brand_name": description_values.get("brand_name"),
+                    "country": description_values.get("country"),
+                    "facet_name": facet_name,
+                    "facet_value": facet_value,
+                },
             )
             html.append(
                 {

i18n/knowledge-panel.pot
@@ -67,21 +67,21 @@ msgstr ""
 #: Eg: Answer questions from Robotoff about the 'beers' category (facet_value = beers, facet_name = category)
 #: Note: No need to add single quotes to facet_value, it's handled internally.
 #, python-brace-format
-msgid "Answer questions from Robotoff about the {facet_value}{facet_name}"
+msgid "Answer questions from Robotoff about the {facet_value} {facet_name}"
 msgstr ""
 
 #: Eg: Answer robotoff questions about the 'beers' category about the 'keystone' brand(facet_value = beers, facet_name = category, brand_name=keystone)
 #: Note: No need to add single quotes to facet_value, it's handled internally.
 #, python-brace-format
-msgid "Answer questions from Robotoff about the {facet_value}{facet_name} and the {brand_name} brand"
+msgid "Answer questions from Robotoff about the {facet_value} {facet_name} and the {brand_name} brand"
 msgstr ""
 
 #, python-brace-format
-msgid "Answer questions from Robotoff about the {facet_value}{facet_name} for the country - {country}"
+msgid "Answer questions from Robotoff about the {facet_value} {facet_name} for the country - {country}"
 msgstr ""
 
 #, python-brace-format
-msgid "Answer questions from Robotoff about the {facet_value}{facet_name} and the {brand_name} brand for the country - {country}"
+msgid "Answer questions from Robotoff about the {facet_value} {facet_name} and the {brand_name} brand for the country - {country}"
 msgstr ""
 
 #, python-brace-format